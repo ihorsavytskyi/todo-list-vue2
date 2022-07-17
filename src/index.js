52d8a1dfe810b64eb0dab045d48c51b2368b3345
Ihor Savytskyi <ihorsavytskyi@gmail.com>
2022-07-17 15:12:33 +0300

    update(*): added i18n

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import App from './app.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
+import VueI18n from 'vue-i18n';
+
+Vue.use(VueI18n);
+
+const i18n = new VueI18n({
+  locale: 'ua',
+  messages: {
+    en: {}
+  }
+})
 
 new Vue({
 	el: "#app",
 	router,
 	store,
 	vuetify,
+	i18n,
 	render: 
 	  (h) => h(App)
 });
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,82 +1,121 @@
 <template>
   <v-container>
-    <h3>Add new todo item</h3>
     <v-container>
-      <v-form>
-        <v-text-field v-model="newTodoItem" label="New todo item" @keydown.enter="addTodo()">
-        </v-text-field>
-        <v-btn rounded color="primary" dark @click="addTodo()">
-          Add
-        </v-btn>
-      </v-form>
-    </v-container>
-    <h3 v-if="getTodoList.length > 0">Have to do : {{getTodoList.length}}</h3>
-    <v-container v-if="getTodoList.length === 0">
-      <v-row no-gutters>
-        <p>Sorry but todo list is empty</p>
+      <v-row class="justify-start">
+        <v-col
+        cols="12"
+        sm="8"
+        md="8">
+          <h3> {{ $t('text1') }}</h3>
+          <v-form>
+            <v-text-field v-model="newTodoItem" label="New todo item" @keydown.enter="addTodo()">
+            </v-text-field>
+          </v-form>
+        </v-col>
+        <v-col
+        cols="12"
+        sm="4"
+        md="4"
+        class="align-self-center"
+        >
+          <v-btn rounded color="primary" dark @click="addTodo()">
+            {{ $t('button1') }}
+          </v-btn>
+        </v-col>
       </v-row>
     </v-container>
-    <v-container v-else>
-      <v-row class="justify-start">
-        <v-list v-for="todo in getTodoList" :key="todo.id">
-          <v-list-item>
-            <v-list-item-content class="pa-2">
-              <v-card class="mx-auto my-1" max-width="374">
-                <v-card-title>
-                  {{ todo.name }}
-                </v-card-title>
-                <v-card-actions>
-                  <v-btn class="ma-2" color="primary" dark @click="сomplete(todo)">
-                    Done
-                    <v-icon dark right>
-                      mdi-checkbox-marked-circle
-                    </v-icon>
-                  </v-btn>
-                  <v-btn dark class="success" text @click="edit(todo)">
-                    Edit
-                    <v-icon dark right>
-                      mdi-pencil
+    <h3 v-if="getTodoList.length > 0">{{ $t('text2') }} : {{getTodoList.length}}</h3>
+    <v-container no-gutters v-if="getTodoList.length === 0">
+      <v-row>
+        <v-col>
+          <p>{{ $t('text3') }}</p>
+        </v-col>
+      </v-row>
+    </v-container>
+    <div class="todo-list" v-else>
+      <v-list v-for="todo in getTodoList" :key="todo.id">
+        <v-list-item>
+          <v-list-item-content class="pa-1">
+            <v-card class="mx-auto my-1" max-width="374">
+              <v-card-title>
+                {{ todo.name }}
+              </v-card-title>
+              <v-card-actions>
+                <v-btn class="ma-2" color="primary" dark @click="сomplete(todo)">
+                  {{ $t('button2') }}
+                  <v-icon dark right>
+                    mdi-checkbox-marked-circle
                   </v-icon>
-                  </v-btn>
-                  <v-btn class="ma-2" color="red" dark @click="removeItem(todo)">
-                    Remove
-                    <v-icon dark right>
-                      mdi-cancel
-                    </v-icon>
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-row>
-    </v-container>
-    <h3 v-if="getComplitedItems.length > 0">Completed : {{ getComplitedItems.length }}</h3>
-    <v-container>
-      <v-row class="justify-start">
-        <v-list v-for="todo in getComplitedItems" :key="todo.id">
-          <v-list-item>
-            <v-list-item-content class="pa-2">
-              <v-card class="mx-auto my-1" max-width="374">
-                <v-card-title>
-                  {{ todo.name }}
-                </v-card-title>
-                <v-card-actions>
-                  <v-btn class="ma-2" color="orange darken-2" dark  @click="returnItem(todo)">
-                    <v-icon dark left>
-                      mdi-arrow-left
-                    </v-icon>
-                    Return
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-row>
-    </v-container>
+                </v-btn>
+                <v-btn dark class="success" text @click="edit(todo)">
+                  {{ $t('button3') }}
+                  <v-icon dark right>
+                    mdi-pencil
+                </v-icon>
+                </v-btn>
+                <v-btn class="ma-2" color="red" dark @click="removeItem(todo)">
+                  {{ $t('button4') }}
+                  <v-icon dark right>
+                    mdi-cancel
+                  </v-icon>
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </div>
+    <h3 v-if="getComplitedItems.length > 0">{{ $t('text4') }} : {{ getComplitedItems.length }}</h3>
+    <div class="todo-list">
+      <v-list v-for="todo in getComplitedItems" :key="todo.id">
+        <v-list-item>
+          <v-list-item-content class="pa-1">
+            <v-card class="mx-auto my-1" max-width="374">
+              <v-card-title>
+                {{ todo.name }}
+              </v-card-title>
+              <v-card-actions>
+                <v-btn class="ma-2" color="orange darken-2" dark  @click="returnItem(todo)">
+                  <v-icon dark left>
+                    mdi-arrow-left
+                  </v-icon>
+                  {{ $t('button5') }}
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </div>
   </v-container>
 </template>
+
+<i18n>
+{
+  "en": {
+    "text1": "Add new todo item",
+    "text2": "Have to do",
+    "text3": "Sorry but todo list is empty",
+    "text4": "Completer",
+    "button1": "Add",
+    "button2": "Done",
+    "button3": "Edit",
+    "button4": "Remove",
+    "button5": "Return"
+  },
+  "ua": {
+    "text1": "Додати новий запис",
+    "text2": "Необхідно зробити",
+    "text3": "Вибачте, але список завдань до виконання порожний",
+    "text4": "Виконано",
+    "button1": "Додати",
+    "button2": "Виконано",
+    "button3": "Змінити",
+    "button4": "Видалити",
+    "button5": "Повернути до виконання"
+  }
+}
+</i18n>
 
 <script>
 import { mapGetters } from 'vuex';
@@ -88,12 +127,7 @@ export default {
       newTodoItem: '',
     };
   },
-  computed: {
-    ...mapGetters(['getTodoList', 'getComplitedItems', 'getComlitedTodoItemsCount']),
-    getNewTodo() {
-      return this.$store.newItem;
-    }
-  },
+  computed: mapGetters(['getTodoList', 'getComplitedItems', 'getComlitedTodoItemsCount']),
   methods: {
     edit(todo) {
       console.log('edit :', todo.id);
@@ -116,6 +150,11 @@ export default {
 </script>
 
 <style lang="less" scoped>
+  .todo-list {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 16px 0;
+  }
   .v-list {
     flex: 0 0 50%;
     .v-card {
